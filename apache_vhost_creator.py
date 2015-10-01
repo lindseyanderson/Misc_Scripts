8da1d654edadca42d4a9fe32c13485b7b07b19fc
lindseyanderson <lindsey.anderson@rackspace.com>
2015-10-01 19:14:54 +0000

    Added initial virtualhost class

--- apache_vhost_creator.py
+++ apache_vhost_creator.py
@@ -80,6 +80,73 @@ class Server:
               self.apache_vhost_config_dir)
    
 
+class VirtualHost:
+    """
+    """
+    def __init__(self, args):
+        self._args = args
+        self.server_name = self.get_server_name()
+        self.server_aliases = self.get_server_aliases() 
+        self.document_root = self.get_document_root()
+
+    def get_server_name(self):
+        self.server_name = self._args.get('server_name')
+        return self.server_name
+
+    def get_server_aliases(self):
+        server_aliases = self._args.get('server_aliases')
+        self.server_aliases = ' '.join(map(str, server_aliases)) \
+                              if server_aliases else \
+                              'www.{0}'.format(self.server_name)
+        return self.server_aliases
+    def get_document_root(self):
+        document_root = self._args.get('document_root')
+        self.document_root = '/var/www/vhosts/{0}/httpdocs'.format( 
+                             self.server_name) if not document_root else \
+                             document_root
+        return self.document_root
+
+
 if __name__ == '__main__':
+    description = "Virtual Host installation for Apache running on Linux."
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-s', '--server-name',
+                        help="ServerName to be used in configuration.",
+                        required=True)
+    parser.add_argument('-a', '--server-aliases', nargs='*',
+                        help="List of alternate names")
+    parser.add_argument('-d', '--document-root', 
+                        help="Location for all files to be stored.")
+    parser.add_argument('-b', '--bind-address', default='*',
+                        help="IP the virtual host will listen on.")
+    parser.add_argument('-l', '--log-directory',
+                        help="Log directory location on the server.")
+    parser.add_argument('-p', '--http-port', 
+                        help="HTTP port for the virtual host to listen on.",
+                        default=80)
+    parser.add_argument('-r', '--reload-service', action='store_true',
+                        help="Reload the service after installation.",
+                        default=False)
+    parser.add_argument('-z', '--no-install', action='store_true',
+                        help="Only display virtual host data, do not install.",
+                        default=False)
+    parser.add_argument('--https-port', default=443,
+                        help="HTTPS port for the virtual host to listen on.")
+    parser.add_argument('--ssl-certificate-file',
+                        help="Location to the SSL Certificate file.")
+    parser.add_argument('--ssl-certificate-key-file',
+                        help="Location to the SSL Certificate Key file.")
+    parser.add_argument('--ssl-certificate-ca-file',
+                        help="Location to the SSL Certificate CA file.")
+    parser.add_argument('--enable-ssl', action='store_true',
+                        help="Enable SSL virtual host.", default=False)
+
+    args = vars(parser.parse_args())
+
     my_server = Server()
+    virtualhost = VirtualHost(args)
     my_server.print_about()
+    print virtualhost.server_name
+    print virtualhost.server_aliases
+    print virtualhost.document_root
+
